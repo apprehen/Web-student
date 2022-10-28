70ffc5ece06092e8d471517538e452de9ff45f16
apprehen <3514392356@qq.com>
2022-10-28 11:26:06 +0800

    updage

--- Node/03_包管理器/01_path.js
+++ Node/03_包管理器/01_path.js
@@ -13,7 +13,23 @@
                     - 在网络中
                         http://www.xxxx/...
                         https://www.xxx/...
+								- 如果直接调用resolve，则返回当前的工作目录
+									- 注意我们通过不同的方式执行代码，它的工作目录可能会不一样 (使用__dirname)
+								- 如果将一个相对路径作为参数
+									- 则resolve会自动将其转换为绝对路径
 
+								- 一般会将一个绝对路径作为第一个参数
+									一个相对路径作为第二个参数
+									这样他会自动计算出最终路径
 */
 const path = require("node:path")
-console.log(path)
+// 不会这样写，调用方式不同生成路径不同
+// const result = path.resolve("./hello.js")
+// const result = path.resolve(
+// 	"E:\\Web\\Web-student\\Node\\03_包管理器",
+// 	"./Hello.js"
+// )
+
+// 最终形态,好使,不会根据系统不同而不一样,保证正常运行
+const result = path.resolve(__dirname,'./hello.js')
+console.log(result)
