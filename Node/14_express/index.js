5f2604ae5ef44edbc63cea27429c8b401121dc1c
apprehen <3514392356@qq.com>
2022-11-14 13:23:14 +0800

    update

--- Node/14_express/index.js
+++ Node/14_express/index.js
@@ -6,16 +6,44 @@ const session = require("express-session")
 
 const userRouter = require("./routes/user")
 const goodsRouter = require("./routes/goods")
-
+// 引入session存储工具的包
+const FileStore = require("session-file-store")(session)
 app.set("view engine", "ejs")
 app.set("views", path.resolve(__dirname, "views"))
-
 app.use(express.static(path.resolve(__dirname, "public")))
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(session({
-    secret:"dazhaxie"
+    secret:"megumi",
+		store: new FileStore({})
 }))
+
+/*
+  session 是服务器创建的一个对象，这个对象用来储存用户的信息
+		- 每一个session都会有一个唯一的id，session创建后id会以cookie的形式发送给浏览器
+	浏览器收到以后，每次访问都会将id发回，服务器中就可以根据id找到对应的session
+
+	id(cookie) --------> session 对象
+	session什么时候会失效？
+		第一种：浏览器的cookie没了
+		第二种：服务器中的session对象没了
+
+	express-session 默认是将session存储在内存中，所以服务器一旦重启session就失效辣
+		考虑使用session通常会对session进行一个持久化的操作(写到文件中或者数据库里)
+	
+	如果将session存储到文件中
+		- 需要引入一个中间件 session-file-store
+		- 使用步骤：
+				1.安装
+					yarn add session-file-store
+				2.引入
+					const FileStore = require("session-file-store")(session)
+				3.设置为中间件
+				app.use(session({
+						secret:"megumi",
+						store: new FileStore({})
+				}))
+*/ 
 
 app.use("/students", require("./routes/student"))
 
